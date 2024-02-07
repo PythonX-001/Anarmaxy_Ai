81d9a2aa87f8af37189baa7044b883513dd8a1ad
No0ne003 <hi.yassir.op@gmail.com>
2024-02-07 15:21:11 +0100

    improve side bar toggel on big screen

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,7 @@
 // Select the aside element
 const aside = document.querySelector(".primary-aside");
 
-// Mobile toggle
+//* Mobile toggle
 const asideToggleMobile = document.querySelector(".mobile-aside-toggle");
 
 asideToggleMobile.addEventListener("click", () => {
@@ -16,12 +16,11 @@ asideToggleMobile.addEventListener("click", () => {
   }
 });
 
-
-// Desktop toggle
+//* Desktop toggle
 const asideToggleDesktop = document.querySelector(".desktop-aside-toggle");
 
-asideToggleDesktop.addEventListener("mouseenter", () => toggleOpacity(80));
-asideToggleDesktop.addEventListener("mouseleave", () => toggleOpacity(100));
+asideToggleDesktop.addEventListener("mouseenter", () => toggleOpacity());
+asideToggleDesktop.addEventListener("mouseleave", () => toggleOpacity());
 asideToggleDesktop.addEventListener("click", () => {
   // Toggle visibility for desktop
   toggleVisibility(asideToggleDesktop, "data-visibleD");
@@ -29,6 +28,7 @@ asideToggleDesktop.addEventListener("click", () => {
   toggleDesktopIconAndStyle();
 });
 
+//* Functions
 // Function to toggle visibility and ARIA attribute
 function toggleVisibility(toggleElement, dataAttribute) {
   const isVisible = aside.getAttribute(dataAttribute) === "true";
@@ -37,9 +37,8 @@ function toggleVisibility(toggleElement, dataAttribute) {
 }
 
 // Function to toggle opacity based on the provided value
-function toggleOpacity(opacityValue) {
-  aside.classList.toggle('opacity-100', opacityValue === 100);
-  aside.classList.toggle('opacity-80', opacityValue === 80);
+function toggleOpacity() {
+  aside.classList.toggle("toggleOpacity");
 }
 
 // Function to toggle desktop icon and style
@@ -48,13 +47,22 @@ function toggleDesktopIconAndStyle() {
   asideToggleDesktop.innerHTML = isVisible ? ">>" : "<<";
   const rightClass = isVisible ? "-right-5" : "-right-3";
   const topClass = isVisible ? "top-[90px]" : "top-[70px]";
-  asideToggleDesktop.classList.replace(isVisible ? "-right-3" : "-right-5", rightClass);
-  asideToggleDesktop.classList.replace(isVisible ? "top-[70px]" : "top-[90px]", topClass);
+  asideToggleDesktop.classList.replace(
+    isVisible ? "-right-3" : "-right-5",
+    rightClass,
+  );
+  asideToggleDesktop.classList.replace(
+    isVisible ? "top-[70px]" : "top-[90px]",
+    topClass,
+  );
 }
 
 // Function to close aside when clicking outside
 function closeAsideOnClickOutside(event) {
-  if (!aside.contains(event.target) && asideToggleMobile.getAttribute("aria-expanded") === "true") {
+  if (
+    !aside.contains(event.target) &&
+    asideToggleMobile.getAttribute("aria-expanded") === "true"
+  ) {
     toggleVisibility(asideToggleMobile, "data-visibleM");
     document.body.removeEventListener("click", closeAsideOnClickOutside);
   }
