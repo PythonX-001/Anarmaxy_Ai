ca8bf1613f7a20f5beefe5af9c4441a182f80bb6
No0ne003 <hi.yassir.op@gmail.com>
2024-02-14 14:42:47 +0100

    make close/open data nav on local storage

--- js/nav.js
+++ js/nav.js
@@ -1,32 +1,26 @@
 // Select the aside element
 const aside = document.querySelector(".chat-sidebar");
-
-//! Mobile toggle
 const asideToggleMobile = document.querySelector(".mobile-aside-toggle");
-
-asideToggleMobile.addEventListener("click", () => {
-  // Toggle visibility for mobile
-  toggleVisibility(asideToggleMobile, "data-visibleM");
-
-  // Add or remove event listener to close aside on outside click
-  if (asideToggleMobile.getAttribute("aria-expanded") === "true") {
-    document.body.addEventListener("click", closeAsideOnClickOutside);
-  } else {
-    document.body.removeEventListener("click", closeAsideOnClickOutside);
-  }
-});
-
-//! Desktop toggle
 const asideToggleDesktop = document.querySelector(".desktop-aside-toggle");
 
-asideToggleDesktop.addEventListener("mouseenter", () => toggleOpacity());
-asideToggleDesktop.addEventListener("mouseleave", () => toggleOpacity());
-asideToggleDesktop.addEventListener("click", () => {
+//! Local Storage
+const localStorageClosedNav = localStorage.getItem("navClose");
+
+if (localStorageClosedNav === "true") {
   // Toggle visibility for desktop
   toggleVisibility(asideToggleDesktop, "data-visibleD");
   // Toggle desktop icon and style
   toggleDesktopIconAndStyle();
-});
+}
+
+//! close/open Nav
+//! Mobile toggle
+asideToggleMobile.addEventListener("click", toggleMobileVisibility);
+
+//! Desktop toggle
+asideToggleDesktop.addEventListener("mouseenter", toggleOpacity);
+asideToggleDesktop.addEventListener("mouseleave", toggleOpacity);
+asideToggleDesktop.addEventListener("click", toggleDesktopVisibility);
 
 //! Functions
 // Function to toggle visibility and ARIA attribute
@@ -34,6 +28,7 @@ function toggleVisibility(toggleElement, dataAttribute) {
   const isVisible = aside.getAttribute(dataAttribute) === "true";
   aside.setAttribute(dataAttribute, !isVisible);
   toggleElement.setAttribute("aria-expanded", !isVisible);
+  localStorage.setItem("navClose", !isVisible);
 }
 
 // Function to toggle opacity based on the provided value
@@ -66,4 +61,25 @@ function closeAsideOnClickOutside(event) {
     toggleVisibility(asideToggleMobile, "data-visibleM");
     document.body.removeEventListener("click", closeAsideOnClickOutside);
   }
+}
+
+// Function to toggle mobile visibility
+function toggleMobileVisibility() {
+  // Toggle visibility for mobile
+  toggleVisibility(asideToggleMobile, "data-visibleM");
+
+  // Add or remove event listener to close aside on outside click
+  if (asideToggleMobile.getAttribute("aria-expanded") === "true") {
+    document.body.addEventListener("click", closeAsideOnClickOutside);
+  } else {
+    document.body.removeEventListener("click", closeAsideOnClickOutside);
+  }
+}
+
+// Function to toggle desktop visibility
+function toggleDesktopVisibility() {
+  // Toggle visibility for desktop
+  toggleVisibility(asideToggleDesktop, "data-visibleD");
+  // Toggle desktop icon and style
+  toggleDesktopIconAndStyle();
 }
